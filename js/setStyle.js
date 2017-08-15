447d946562ebdbc03823cec453379bb2c1ca36ae
T&H <truemenhale@gmail.com>
2017-08-15 17:39:05 +0800

    step1

---
+++ js/setStyle.js
@@ -0,0 +1,8 @@
+/**
+ * Created by Zain on 15/8/2017.
+ */
+$(function(){
+   aLi = $('.steps_container').find('li');
+    aLi.css({'height':$(window).width()*0.1473});
+    $('.steps_container').css('display','block')
+});
