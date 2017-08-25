6beeb12a6797328fb2b67784acf339848ffa4558
T&H <truemenhale@gmail.com>
2017-08-25 17:37:51 +0800

    codingwall

--- js/codingwall.js
+++ js/codingwall.js
@@ -1,3 +1,37 @@
 /**
  * Created by Zain on 25/8/2017.
  */
+$(function(){
+    var w = $(document.body).outerWidth(true);
+    var h = $(document.body).outerHeight(true);
+    var l_num = Math.ceil(w/8);
+    var c_num = Math.ceil(h/18);
+    var CC = $('.CodeRainBox');
+    var num_arr = [];
+    for(var i = 0; i < l_num; i++){
+        for(var j = 0; j < c_num ; j++){
+            if(Math.random() <= 0.5){
+                num_arr.push(0)
+            }else {
+                num_arr.push(1)
+            }
+        }
+    }
+    var coding = num_arr.toString().replace(/,/g, "");
+    setInterval(
+        function(){
+                for(var i = 0; i < num_arr.length; i++){
+                    if(Math.random() <= 0.5){
+                        if (num_arr[i] == 1){
+                            num_arr[i] = 0;
+                        }else {
+                            num_arr[i] = 1;
+                        }
+                    }
+                }
+            var coding = num_arr.toString().replace(/,/g, "");
+            CC.html(coding)
+        }
+    ,150);
+    CC.html(coding)
+});
